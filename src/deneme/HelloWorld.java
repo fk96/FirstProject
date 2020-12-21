0926cdd0b64a82bde9e6c764416998b5d47d38d3
Furkan Kale <furkankale.1996@hotmail.com>
2020-12-21 10:04:49 +0300

    ilk commit

---
+++ src/deneme/HelloWorld.java
@@ -0,0 +1,17 @@
+package deneme;
+import java.util.Scanner;
+public class HelloWorld {
+	final int i=10;
+	public static void main(String[] args) {
+		
+		int x=10;
+		System.out.println(x++);
+		
+		int y=5;
+		++y;
+		
+		
+		System.out.println(x+y);
+	}
+
+}
